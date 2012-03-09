6362ff1e4396c13438b5837be4d82ec5e40d9497
Jesse Campbell <hikeit@gmail.com>
2012-03-09 10:48:55 -0500

    add --purge option shamelessly stolen from knife ec2
    currently expects vmname to match node and client name

--- lib/chef/knife/vsphere_vm_delete.rb
+++ lib/chef/knife/vsphere_vm_delete.rb
@@ -7,12 +7,34 @@ require 'chef/knife'
 require 'chef/knife/BaseVsphereCommand'
 require 'rbvmomi'
 
+# These two are needed for the '--purge' deletion case
+require 'chef/node'
+require 'chef/api_client'
+
 # Delete a virtual machine from vCenter
 class Chef::Knife::VsphereVmDelete < Chef::Knife::BaseVsphereCommand
 
 	banner "knife vsphere vm delete VMNAME"
 
+	option :purge,
+		:short => "-P",
+		:long => "--purge",
+		:boolean => true,
+		:default => false,
+		:description => "Destroy corresponding node and client on the Chef Server, in addition to destroying the EC2 node itself."
+
 	get_common_options
+
+	# Extracted from Chef::Knife.delete_object, because it has a
+	# confirmation step built in... By specifying the '--purge'
+	# flag (and also explicitly confirming the server destruction!)
+	# the user is already making their intent known. It is not
+	# necessary to make them confirm two more times.
+	def destroy_item(itemClass, name, type_name)
+		object = itemClass.load(name)
+		object.destroy
+		puts "Deleted #{type_name} #{name}"
+	end
 
 	def run
 		$stdout.sync = true
@@ -35,5 +57,11 @@ class Chef::Knife::VsphereVmDelete < Chef::Knife::BaseVsphereCommand
 		vm.Destroy_Task
 		puts "Deleted virtual machine #{vmname}"
 
+		if config[:purge]
+			destroy_item(Chef::Node, vmname, "node")
+			destroy_item(Chef::ApiClient, vmname, "client")
+		else
+			puts "Corresponding node and client for the #{vmname} server were not deleted and remain registered with the Chef Server"
+		end
 	end
 end
