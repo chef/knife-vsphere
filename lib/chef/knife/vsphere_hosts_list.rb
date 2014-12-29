66d1b752d318f92ef304b5c0aa568d0cb2b2672e
Ezra Pagel <ezra.pagel@gmail.com>
2014-12-29 15:39:51 -0600

    by default, show hosts from all pools

--- lib/chef/knife/vsphere_hosts_list.rb
+++ lib/chef/knife/vsphere_hosts_list.rb
@@ -2,6 +2,7 @@ require 'chef/knife'
 require 'chef/knife/base_vsphere_command'
 require 'rbvmomi'
 require 'netaddr'
+
 #list hosts belonging to pool
 class Chef::Knife::VsphereHostsList < Chef::Knife::BaseVsphereCommand
   banner "knife vsphere hosts list"
@@ -12,38 +13,30 @@ class Chef::Knife::VsphereHostsList < Chef::Knife::BaseVsphereCommand
          :short => "-h",
          :description => "Target pool"
 
-  def traverse_folders_for_pool(folder, poolname)
-    children = folder.children.find_all
-    children.each do |child|
-      if child.class == RbVmomi::VIM::ClusterComputeResource || child.class == RbVmomi::VIM::ComputeResource || child.class == RbVmomi::VIM::ResourcePool 
-        if child.name == poolname then return child end
-      elsif child.class == RbVmomi::VIM::Folder
-        pool = traverse_folders_for_pool(child, poolname)
-        if pool then return pool end
-      end
-    end
-    return false
+  def find_pools(folder, poolname = nil)
+    pools = folder.children.find_all.select {|p| p.is_a?(RbVmomi::VIM::ComputeResource) || p.is_a?(RbVmomi::VIM::ResourcePool) }
+    poolname.nil? ? pools : pools.select {|p| p.name == poolname }
   end
 
   def run
-    poolname = config[:pool]
-    if poolname.nil?
-      show_usage
-      fatal_exit("You must specify a resource pool or cluster name (see knife vsphere pool list)")
-    end
-
-
     vim = get_vim_connection
     dc = get_datacenter
     folder = dc.hostFolder
 
-    pool = traverse_folders_for_pool(folder, poolname) or abort "Pool #{poolname} not found"
+    target_pool = config[:pool]
 
-    hosts=pool.host
-     unless hosts.nil?
+    pools = find_pools(folder, target_pool)
+    if target_pool && pools.empty? 
+      puts "Pool #{target_pool} not found"
+      return
+    end
+
+    pools.each do |pool|
+      puts "#{ui.color("Pool", :cyan)}: #{pool.name}"
+      hosts = pool.host || []
       hosts.each do |hostc|
-      puts "#{ui.color("Host", :cyan)}: #{hostc.name}"
-     end
-   end
-end
+        puts "  #{ui.color("Host", :cyan)}: #{hostc.name}"
+      end
+    end
+  end
 end
