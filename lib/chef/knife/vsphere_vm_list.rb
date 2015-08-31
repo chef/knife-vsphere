4f230c90e9ccd4887e31f9b4a375cfec84baeb1a
brok02 <kelly.brownsberger@csgi.com>
2015-08-31 13:40:22 -0400

    tersing it up (+1 squashed commits)
    Squashed commits:
    
    [cc5ab21] no message (+2 squashed commit)
    
    Squashed commit:
    
    [7218227] fixing rubocop violations per sean
    changing behavior to always perform --only-folder queries recursively
    
    [c32644d] doc states that --only-folders is supported with knife vsphere vm list, but such a switch was missing from the implementation.  this is an implementation of what I believe the spirit of that would be

--- lib/chef/knife/vsphere_vm_list.rb
+++ lib/chef/knife/vsphere_vm_list.rb
@@ -16,19 +16,28 @@ class Chef::Knife::VsphereVmList < Chef::Knife::BaseVsphereCommand
          short: '-r',
          description: 'Recurse into sub-folders'
 
-  def traverse_folders(folder, is_top = false, recurse = false)
-    vms = find_all_in_folder(folder, RbVmomi::VIM::VirtualMachine).select { |v| v.config && !v.config.template }
-    if vms.any?
-      puts "#{ui.color('Folder', :cyan)}: " + (folder.path[3..-1].map { |x| x[1] }.* '/')
-      vms.each { |v|  print_vm(v) }
-    elsif is_top
-      puts "#{ui.color('No VMs', :cyan)}"
+  option :onlyfolders,
+         long: '--only-folders',
+         description: 'List only the folders found',
+         boolean: false
+
+  def traverse_folders(folder, is_top = false, recurse = false, only_folders = false)
+    if only_folders
+      print_folder(folder)
+    else
+      vms = find_all_in_folder(folder, RbVmomi::VIM::VirtualMachine).select { |v| v.config && !v.config.template }
+      if vms.any?
+        print_folder(folder)
+        vms.each { |v| print_vm(v) }
+      elsif is_top
+        puts "#{ui.color('No VMs', :cyan)}"
+      end
     end
 
     return unless recurse
     folders = find_all_in_folder(folder, RbVmomi::VIM::Folder)
     folders.each do |child|
-      traverse_folders(child, false, recurse)
+      traverse_folders(child, false, recurse, only_folders)
     end
   end
 
@@ -47,11 +56,16 @@ class Chef::Knife::VsphereVmList < Chef::Knife::BaseVsphereCommand
     puts "\t\t#{ui.color('State:', :magenta)} #{state}"
   end
 
+  def print_folder(folder)
+    puts "#{ui.color('Folder', :cyan)}: " + (folder.path[3..-1].map { |x| x[1] }.* '/')
+  end
+
   def run
     vim_connection
     base_folder = find_folder(get_config(:folder))
-    recurse =  get_config(:recursive)
+    only_folders = get_config(:onlyfolders)
+    recurse = only_folders || get_config(:recursive)
     is_top = true
-    traverse_folders(base_folder, is_top, recurse)
+    traverse_folders(base_folder, is_top, recurse, only_folders)
   end
 end
