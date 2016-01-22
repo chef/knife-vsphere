69b8947c39c5ddbe85bac8474efa59156446123d
Sean Walberg <sean@ertw.com>
2016-01-21 20:12:51 -0500

    Add a spec for clone and clean up others

--- spec/vlan_list_spec.rb
+++ spec/vlan_list_spec.rb
@@ -2,22 +2,20 @@ require 'spec_helper'
 require 'chef/knife/vsphere_vlan_list'
 
 describe Chef::Knife::VsphereVlanList do
-
   let(:datacenter) { double('Datacenter') }
   let(:n1) { double('Network', name: 'n1') }
   let(:n2) { double('Network', name: 'n2') }
 
-  it 'enumerates the vlans' do
-    knife = described_class.new
+  subject { described_class.new }
 
-    expect(knife).to receive(:vim_connection)
-    expect(knife).to receive(:datacenter).and_return(datacenter)
+  it 'enumerates the vlans' do
+    expect(subject).to receive(:vim_connection)
+    expect(subject).to receive(:datacenter).and_return(datacenter)
     expect(datacenter).to receive(:network).and_return([n1, n2])
     expect(n1).to receive :name
     expect(n2).to receive :name
-    expect(knife).to receive(:puts).twice
+    expect(subject).to receive(:puts).twice
 
-    knife.run
+    subject.run
   end
-
 end
