40eb0d42144901b31cef87aaaa2401ad8bf154cb
Sean Walberg <sean@ertw.com>
2016-09-07 08:55:56 -0400

    Fix bug with no cmacs or cips; tests; refactor
    I was sent a bug privately where cloning a VM with no --cmacs or
    --cips triggered an abort. When I updated --cmacs to default to `auto` I didn't change that guard clause, so the code always assumed there was a cmac specified. So let's get away from checking strings ;)
    
    Also took this opportunity to dive into the mocking necessary to test a clone. There's a lot of mocking the SUT but much of it is unavoidable. I moved some of the code around so that the mocks were working on smaller pieces of code, and I hope that I'll be able to test those pieces of code more thoroughly.
    
    These tests in the cloning code do set us up to be able to test the clone specs being sent with vmware.

--- knife-vsphere.gemspec
+++ knife-vsphere.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency('chef', ['>= 0.10.0'])
   s.add_development_dependency('rspec')
   s.add_development_dependency('rake')
+  s.add_development_dependency('byebug')
 end
--- spec/vsphere_vm_clone_spec.rb
+++ spec/vsphere_vm_clone_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 require 'chef/knife/vsphere_vm_clone'
 
 describe Chef::Knife::VsphereVmClone do
-  let(:datacenter) { double('Datacenter') }
+  let(:datacenter) { double('Datacenter', vmFolder: empty_folder, hostFolder: empty_folder) }
   let(:vim) { double('VimConnection', serviceContent: service_content) }
   let(:service_content) { double('ServiceContent') }
+  let(:root_folder) { double('RootFolder', children: []) }
+  let(:empty_folder) { double('Folder', childEntity: [], children: []) }
+  let(:host) { double('Host', resourcePool: double('ResourcePool')) }
+  let(:task) { double('Task', wait_for_completion: 'done') }
 
   subject { described_class.new }
 
@@ -12,9 +16,16 @@ describe Chef::Knife::VsphereVmClone do
     subject.config[:random_vmname_prefix] = 'vm-'
     subject.config[:vsphere_pass] = 'password'
     subject.config[:vsphere_host] = 'host'
+    subject.config[:verbosity] = 0
+    subject.config[:customization_ips] = Chef::Knife::VsphereVmClone::NO_IPS
+    subject.config[:customization_macs] = Chef::Knife::VsphereVmClone::AUTO_MAC
   end
 
   context 'input handling' do
+    before do
+      subject.config[:source_vm] = 'my_template'
+    end
+
     it 'requires a vm name' do
       expect { subject.run }.to raise_error SystemExit
     end
@@ -24,5 +35,58 @@ describe Chef::Knife::VsphereVmClone do
       expect(subject).to receive(:vim_connection).and_raise ArgumentError
       expect { subject.run }.to raise_error ArgumentError
     end
+  end
+
+  context 'customizing the mac' do
+    let(:template) { double('Template', config: {}) }
+
+    before do
+      allow(subject).to receive(:vim_connection).and_return(vim)
+      # It is difficult to mock this because the current implementation checks
+      # for explicity RbVmomi class names
+      allow(subject).to receive(:datacenter).and_return(datacenter)
+      allow(subject).to receive(:find_available_hosts).and_return( [host])
+
+      allow(service_content).to receive(:virtualDiskManager) # what does this call actually do?
+      subject.config[:folder] = ''
+      subject.config[:source_vm] = 'my_template'
+    end
+
+    context 'the mac is given' do
+      it 'requires an ip' do
+        subject.name_args = 'foo'
+        subject.config[:customization_macs] = '00:11:22:33:44:55'
+
+        expect { subject.run }.to raise_error SystemExit
+      end
+    end
+
+    context 'the mac is not given' do
+      before do
+        subject.name_args = 'foo'
+
+        allow(subject).to receive(:find_in_folder).and_return(template)
+      end
+
+      it 'runs without specifying an ip' do
+        expect(template).to receive(:CloneVM_Task).and_return(task)
+        expect { subject.run }.to_not raise_error
+      end
+
+      it 'runs while specifying an ip' do
+        subject.config[:customization_ips] = '1.2.3.4'
+
+        expect(template).to receive(:CloneVM_Task).and_return(task)
+        expect { subject.run }.to_not raise_error
+      end
+    end
+  end
+
+  context 'customizations' do
+    context 'skip customizations'
+    context 'determining the hostname'
+    context 'windows clone'
+    context 'linux clone'
+    context 'neither windows or linux'
   end
 end
