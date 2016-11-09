8f90990bd37c9fe7aa07959b8dbebb5e36a4bb7e
coda4096 <scott@backups.net.au>
2016-11-09 15:43:10 +0800

    changes for houndci

--- lib/chef/knife/vsphere_vm_vnc_set.rb
+++ lib/chef/knife/vsphere_vm_vnc_set.rb
@@ -21,7 +21,6 @@ class Chef::Knife::VsphereVmVncset < Chef::Knife::BaseVsphereCommand
       fatal_exit('Specify the port to use for VNC')
     end
 
-
     vnc_password = @name_args[2]
     if vnc_password.nil?
       fatal_exit('Specify the password for VNC')
@@ -34,16 +33,16 @@ class Chef::Knife::VsphereVmVncset < Chef::Knife::BaseVsphereCommand
 
     vm = find_in_folder(folder, RbVmomi::VIM::VirtualMachine, vmname) || abort("VM #{vmname} not found")
 
-    extra_config, = vm.collect('config.extraConfig')   
+    extra_config, = vm.collect('config.extraConfig')
 
     vm.ReconfigVM_Task(:spec => {
       :extraConfig => [
-        { :key => 'RemoteDisplay.vnc.enabled', :value => 'true' },
-        { :key => 'RemoteDisplay.vnc.port', :value => vnc_port.to_s },
-        { :key => 'RemoteDisplay.vnc.password', :value => vnc_password.to_s }
+        { key: 'RemoteDisplay.vnc.enabled', value: 'true' },
+        { key: 'RemoteDisplay.vnc.port', value: vnc_port.to_s },
+        { key: 'RemoteDisplay.vnc.password', value: vnc_password.to_s }
       ]
     }).wait_for_completion
 
-    puts extra_config.detect { |x| 'RemoteDisplay.vnc.enabled'.casecmp(x.key) && x.value.downcase == 'true' }
+    puts extra_config.detect { |x| 'RemoteDisplay.vnc.enabled'.casecmp(x.key) && 'true'.casecmp(x.value.downcase) }
   end
 end
